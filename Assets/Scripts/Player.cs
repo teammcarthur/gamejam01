1d2215a6df79bc0fc4caa4bb1e839969ac8e0ba7
teammcarthur <chrism.23092@obhs.school.nz>
2025-07-02 16:36:27 +1200

    Better Gravity & Ground Check

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ public class Player : MonoBehaviour
 {
     int input;
     public int speed;
+    bool grounded;
+    public float CT;
+    float TLG; // Time left ground
     [SerializeField] Rigidbody2D rb;
 
     void Start()
@@ -35,9 +38,27 @@ public class Player : MonoBehaviour
 
     void Jump()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        
+        if (Keyboard.current.spaceKey.isPressed && grounded)
         {
             rb.linearVelocityY = 10;
+            grounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = false;
         }
     }
 }
