07aa667d89aa4c465874e64c0ce12517a2c2b21a
teammcarthur <chrism.23092@obhs.school.nz>
2025-07-03 13:25:36 +1200

    ArtGround

--- Assets/Scripts/PlayButton.cs
+++ Assets/Scripts/PlayButton.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
+
 {
-    void LoadGame()
+    public void LoadGame()
     {
         Debug.Log("PlayButtonPressed");
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Platfroms");
     }
 }
