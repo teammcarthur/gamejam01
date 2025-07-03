a20b44473940b830d57407f8e867454e739fe6c1
teammcarthur <chrism.23092@obhs.school.nz>
2025-07-03 17:59:11 +1200

    CAMERA!

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Camera : MonoBehaviour
+{
+    public float smoothMult, lookMult;
+    public Transform player;
+    Vector3 targetPos, smoothedPos, offset;
+    Vector2 inputNormal;
+    int inputX, inputY;
+    void Update()
+    {
+        GetDirection();
+    }
+
+    void GetDirection()
+    {
+        inputX = (Keyboard.current.rightArrowKey.isPressed ? 1 : 0) + (Keyboard.current.leftArrowKey.isPressed ? -1 : 0);
+        inputY = (Keyboard.current.upArrowKey.isPressed? 1 : 0) + (Keyboard.current.downArrowKey.isPressed? -1 : 0);
+    }
+
+    void FixedUpdate()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        inputNormal = new Vector2(inputX, inputY).normalized;
+        offset = new Vector3(inputNormal.x, inputNormal.y, 0) * lookMult;
+        targetPos = player.position + offset;
+        smoothedPos = Vector3.Lerp(transform.position, targetPos, smoothMult);
+        transform.position = smoothedPos + new Vector3 (0, 0, -10);
+    }
+}
